fad8202933ca4e4fc309a7a865e79664a4df17e8
Anton Todorov <antontodorov321@gmail.com>
2025-01-22 01:14:04 +0200

    Implement custom reversed list

---
+++ 03. Linear-Data-Structures-Exercise-Skeleton/Test/Program.cs
@@ -0,0 +1,11 @@
+﻿var list = new Problem02.DoublyLinkedList.DoublyLinkedList<int>();
+
+list.AddFirst(1);
+list.AddFirst(2);
+list.AddFirst(3);
+list.AddLast(4);
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
