28d6e0e1a491488926065b936546d186c231c71d
Anton Todorov <antontodorov321@gmail.com>
2025-01-27 00:20:58 +0200

    Implement tree methods

--- 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Program.cs
+++ 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Program.cs
@@ -1,19 +1,27 @@
 ﻿namespace Tree
 {
+    using System;
+
     public class Program
     {
         public static void Main(string[] args)
         {
             Tree<int> tree = new Tree<int>(34,
                                  new Tree<int>(36,
-                                    new Tree<int>(42),  
+                                    new Tree<int>(42),
                                     new Tree<int>(3,
                                         new Tree<int>(78)
                                     )
                                  ),
-                                 new Tree<int>(1),
+                                 new Tree<int>(1,
+                                     new Tree<int>(6)
+                                 ),
                                  new Tree<int>(103)
                              );
+
+            Console.WriteLine(string.Join(", ", tree.OrderDfs()));
+            tree.Swap(78, 6);
+            Console.WriteLine(string.Join(", ", tree.OrderDfs()));
         }
     }
 }
--- 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Tree.cs
+++ 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Tree.cs
@@ -5,14 +5,14 @@
 
     public class Tree<T> : IAbstractTree<T>
     {
-        private readonly List<Tree<T>> currentChildren;
+        private readonly List<Tree<T>> children;
         private Tree<T> parent;
         private T value;
 
         public Tree(T value)
         {
             this.value = value;
-            this.currentChildren = new List<Tree<T>>();
+            this.children = new List<Tree<T>>();
         }
 
         public Tree(T value, params Tree<T>[] children)
@@ -21,33 +21,149 @@
             foreach (var child in children)
             {
                 child.parent = this;
-                this.currentChildren.Add(child);
+                this.children.Add(child);
             }
         }
 
         public void AddChild(T parentKey, Tree<T> child)
         {
-            throw new NotImplementedException();
+            Tree<T> parentNode = this.FindNodeWithBfs(parentKey);
+
+            if (parentNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            parentNode.children.Add(child);
+            child.parent = parentNode;
         }
 
         public IEnumerable<T> OrderBfs()
         {
-            throw new NotImplementedException();
+            List<T> result = new List<T>();
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                Tree<T> subtree = queue.Dequeue();
+                result.Add(subtree.value);
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
         }
 
         public IEnumerable<T> OrderDfs()
         {
-            throw new NotImplementedException();
+            Stack<Tree<T>> stack = new Stack<Tree<T>>();
+            Stack<T> result = new Stack<T>();
+            stack.Push(this);
+
+            while (stack.Count > 0)
+            {
+                Tree<T> subtree = stack.Pop();
+
+                foreach (var child in subtree.children)
+                {
+                    stack.Push(child);
+                }
+
+                result.Push(subtree.value);
+            }
+
+            return result;
         }
 
         public void RemoveNode(T nodeKey)
         {
-            throw new NotImplementedException();
+            Tree<T> nodeToBeDeleted = this.FindNodeWithBfs(nodeKey);
+            if (nodeToBeDeleted == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Tree<T> parentNode = nodeToBeDeleted.parent;
+            if (parentNode == null)
+            {
+                throw new ArgumentException();
+            }
+
+            parentNode.children.Remove(nodeToBeDeleted);
         }
 
         public void Swap(T firstKey, T secondKey)
         {
-            throw new NotImplementedException();
+            Tree<T> firsNode = this.FindNodeWithDfs(firstKey);
+            Tree<T> secondNode = this.FindNodeWithDfs(secondKey);
+
+            if (firsNode is null || secondNode is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Tree<T> parentFirstNode = firsNode.parent;
+            Tree<T> parentSecondNode = secondNode.parent;
+
+            if (parentFirstNode is null || parentSecondNode is null)
+            {
+                throw new ArgumentException();
+            }
+
+            int indexFirstChild = parentFirstNode.children.IndexOf(firsNode);
+            int indexSecondChild = parentSecondNode.children.IndexOf(secondNode);
+
+            parentFirstNode.children[indexFirstChild] = secondNode;
+            parentSecondNode.children[indexSecondChild] = firsNode;
+        }
+
+        private Tree<T> FindNodeWithDfs(T firstKey)
+        {
+            Stack<Tree<T>> stack = new Stack<Tree<T>>();
+            stack.Push(this);
+
+            while (stack.Count > 0)
+            {
+                Tree<T> subtree = stack.Pop();
+
+                if (subtree.value.Equals(firstKey))
+                {
+                    return subtree;
+                }
+
+                foreach (var child in subtree.children)
+                {
+                    stack.Push(child);
+                }
+            }
+
+            return null;
+        }
+
+        private Tree<T> FindNodeWithBfs(T parentKey)
+        {
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                Tree<T> subtree = queue.Dequeue();
+                if (subtree.value.Equals(parentKey))
+                {
+                    return subtree;
+                }
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
         }
     }
 }
