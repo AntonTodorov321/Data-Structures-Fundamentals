4609223bf15b1779f18ad861b2e2110ff14bb3ee
Anton Todorov <antontodorov321@gmail.com>
2025-01-26 12:54:06 +0200

    Implement tree constructor

---
+++ 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>(34,
+                                 new Tree<int>(36,
+                                    new Tree<int>(42),  
+                                    new Tree<int>(3,
+                                        new Tree<int>(78)
+                                    )
+                                 ),
+                                 new Tree<int>(1),
+                                 new Tree<int>(103)
+                             );
+        }
+    }
+}
