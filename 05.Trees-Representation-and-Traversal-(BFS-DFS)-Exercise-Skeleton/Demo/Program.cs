0288e38391efa82a93abf4a725375f39514ba0f3
Anton Todorov <antontodorov321@gmail.com>
2025-01-29 23:35:03 +0200

    Implement GetPathsWithGivenSum method

--- 05.Trees-Representation-and-Traversal-(BFS-DFS)-Exercise-Skeleton/Demo/Program.cs
+++ 05.Trees-Representation-and-Traversal-(BFS-DFS)-Exercise-Skeleton/Demo/Program.cs
@@ -8,7 +8,7 @@
         static void Main(string[] args)
         {
             string[] input =
-                new string[] { "9 17", "9 4", "9 14", "4 36", "14 53", "14 59", "53 67", "53 73" };
+                new string[] { "9 17", "9 4", "9 14", "4 36", "14 53", "14 26", "53 67", "53 73" };
 
             IntegerTreeFactory factory = new IntegerTreeFactory();
             IntegerTree tree = factory.CreateTreeFromStrings(input);
--- 05.Trees-Representation-and-Traversal-(BFS-DFS)-Exercise-Skeleton/Tree/IntegerTree.cs
+++ 05.Trees-Representation-and-Traversal-(BFS-DFS)-Exercise-Skeleton/Tree/IntegerTree.cs
@@ -12,12 +12,44 @@
 
         public IEnumerable<IEnumerable<int>> GetPathsWithGivenSum(int sum)
         {
-            throw new NotImplementedException();
+            List<List<int>> result = new List<List<int>>();
+
+            LinkedList<int> currentPath = new LinkedList<int>();
+            currentPath.AddLast(this.Key);
+
+            int currentSum = this.Key;
+            this.Dfs(this, result, currentPath, ref currentSum, sum);
+
+            return result;
         }
 
         public IEnumerable<Tree<int>> GetSubtreesWithGivenSum(int sum)
         {
             throw new NotImplementedException();
+        }
+
+        private void Dfs
+            (Tree<int> subtree,
+            List<List<int>> result,
+            LinkedList<int> currentPath,
+            ref int currentSum,
+            int wantedSum)
+        {
+            foreach (var child in subtree.Children)
+            {
+                currentSum += child.Key;
+                currentPath.AddLast(child.Key);
+
+                Dfs(child, result, currentPath, ref currentSum, wantedSum);
+            }
+
+            if (currentSum == wantedSum)
+            {
+                result.Add(new List<int>(currentPath));
+            }
+
+            currentSum -= subtree.Key;
+            currentPath.RemoveLast();
         }
     }
 }
