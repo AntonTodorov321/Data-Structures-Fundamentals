2a04c8c3f3a5930531e941953051afa9bb1272b8
Anton Todorov <antontodorov321@gmail.com>
2025-02-04 22:52:11 +0200

    Fix bug

--- 06.Heaps-BST-Lab-Skeleton/02.BinarySearchTree/BinarySearchTree.cs
+++ 06.Heaps-BST-Lab-Skeleton/02.BinarySearchTree/BinarySearchTree.cs
@@ -5,26 +5,117 @@
     public class BinarySearchTree<T> : IBinarySearchTree<T>
         where T : IComparable<T>
     {
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        private Node root;
         public BinarySearchTree() { }
+
+        private BinarySearchTree(Node node)
+        {
+            this.PreOrderCopy(node);
+        }
 
         public bool Contains(T element)
         {
-            throw new NotImplementedException();
+            return this.FindNode(element) != null;
         }
 
         public void EachInOrder(Action<T> action)
         {
-            throw new NotImplementedException();
+            this.EachInOrder(action, this.root);
         }
 
         public void Insert(T element)
         {
-            throw new NotImplementedException();
+            this.root = this.Insert(element, this.root);
         }
 
         public IBinarySearchTree<T> Search(T element)
         {
-            throw new NotImplementedException();
+            Node node = this.FindNode(element);
+
+            if (node == null)
+            {
+                return null;
+            }
+
+            return new BinarySearchTree<T>(node);
         }
+
+        private void EachInOrder(Action<T> action, Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(action, node.Left);
+            action(node.Value);
+            this.EachInOrder(action, node.Right);
+        }
+
+        private Node Insert(T element, Node node)
+        {
+            if (node == null)
+            {
+                node = new Node(element);
+            }
+            else if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.Insert(element, node.Left);
+            }
+            else if (element.CompareTo(node.Value) > 0)
+            {
+                node.Right = this.Insert(element, node.Right);
+            }
+
+            return node;
+        }
+
+        private Node FindNode(T element)
+        {
+            Node node = this.root;
+
+            while (node != null)
+            {
+                if (element.CompareTo(node.Value) < 0)
+                {
+                    node = node.Left;
+                }
+                else if (element.CompareTo(node.Value) > 0)
+                {
+                    node = node.Right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return node;
+        }
+
+        private void PreOrderCopy(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.Insert(node.Value);
+            this.PreOrderCopy(node.Left);
+            this.PreOrderCopy(node.Right);
+        }
+
     }
 }
--- 06.Heaps-BST-Lab-Skeleton/Demo/Program.cs
+++ 06.Heaps-BST-Lab-Skeleton/Demo/Program.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using _02.BinarySearchTree;
-using _03.MaxHeap;
-
-namespace Demo
+﻿namespace Demo
 {
+    using _02.BinarySearchTree;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var tree = new BinarySearchTree<int>();
+            tree.Insert(8);
+            tree.Insert(4);
+            tree.Insert(2);
+            tree.Insert(6); 
+            tree.EachInOrder(x => Console.Write(x + ", "));
+
+            tree.Search(4);
         }
     }
 }
